a1d8367de72630b00850bf78c4c2004739d2e525
Ashish Krishnam Yadav <yaduvanshiashish1008@gmail.com>
2025-08-16 01:25:37 +0530

    fibonacci series using loop and function generator

--- Recursion/Day4/functionGenerator.js
+++ Recursion/Day4/functionGenerator.js
@@ -23,6 +23,34 @@ var fibGenerator = function*(){
 
 let generator = fibGenerator()
 for (let i = 0; i < 7; i++) {
-    console.log(generator.next())
+    //console.log(generator.next())
+    
+}
+
+//without using recursion
+// yield : yield keyword is used to pause and resume the function
+
+var fibGenerator = function*(){
+   let i=0;
+   let fib = [0,1]
+   while(true){
+     if(i==0){
+        yield 0
+     }
+     else if(i==1){
+        yield 1
+     }else{
+
+         let cur = fib[i-1] + fib[i - 2]
+         fib[i] = cur
+         yield cur
+     }
+     i++
+   }
+}
+
+let generator1 = fibGenerator()
+for (let i = 0; i < 7; i++) {
+    console.log(generator1.next())
     
 }
